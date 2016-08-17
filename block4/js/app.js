4876ae3d61a2c4ebd2fc420809bd65e0be863087
adrozdek <agata.drozdek@gmail.com>
2016-08-17 20:38:30 +0200

    Add files via upload

---
+++ block4/js/app.js
@@ -0,0 +1,24 @@
+var main = function () {
+    $('.icon-menu').click(function () {
+        $('.menu').animate({
+            left: "0px"
+        }, 200);
+
+        $('body').animate({
+            left: "285px"
+        }, 200);
+        $(this).hide();
+    });
+
+    $('.icon-close').click(function () {
+        $('.menu').animate({
+            left: "-285px"
+        }, 200);
+        $('body').animate({
+            left: "0px"
+        }, 200);
+        $('.icon-menu').show();
+    });
+};
+
+$(document).ready(main);
